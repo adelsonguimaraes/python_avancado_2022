7cd85d07cd7f13e46cc64def18c31e748e3378d9
Adelson Monteiro <adelson.monteiro@fpf.br>
2022-04-01 23:03:46 -0400

    5o dia de aula do curso

--- basic/queries.py
+++ basic/queries.py
@@ -1,5 +1,6 @@
-from django.db.models import Value, ExpressionWrapper, F, IntegerField, FloatField, Q, DateField, DurationField, Case, When
-from django.db.models.functions import Now, Cast, ExtractDay
+from django.db.models import Value, ExpressionWrapper, F, IntegerField, FloatField, Q, DateField, DurationField, Case, \
+    When, CharField, Sum, Max, Min, Avg, Count
+from django.db.models.functions import Now, Cast, ExtractDay, LPad, Upper, Lower
 
 from basic import models
 
@@ -120,29 +121,126 @@ def exercicio12():
     # Acima de 2 anos e menor ou igual a 5 anos - Intermediário
     # Acima de 5 anos - Veterano
     return models.Employee.objects.annotate(
-
+        tempo_casa = ExpressionWrapper(
+            F('admission_date') - Now(), output_field=DurationField(),
+        )
+    ).values(
+            'name', 'tempo_casa'
     )
 
-# def exercicio6():
-#     # return models.Employee.objects.filter(salary__range=(1000, 5000))
-#     return models.Employee.objects.filter(salary__gte=1000, sale__modified_at__gte=5000)
-#
-#
-# def exercico7():
-#     return models.Product.objects.annotate(
-#         diff=ExpressionWrapper(F('sale_price') - F('cost_price'), output_field=FloatField())
-#     ).values('name', 'sale_price', 'cost_price', 'diff')
-#
-#
-# def exercicio8():
-#     return models.Employee.objects.exclude(salary__range=(4000, 8000))
-#
-#
-# def exercicio9():
-#     return models.Sale.objects.filter(date__year__range=(2010, 2021))
-#
-#
-# def exercici10():
-#     return models.Employee.objects.annotate(
-#         diff=ExpressionWrapper(Cast(Now(), output_field=DateField()) - F('birth_date'), output_field=DurationField())
-#     )
+
+def exercicio13():
+    # pegar o id e adicionar 5 zeros a esquerda
+    return models.Employee.objects.annotate(
+        code=LPad(
+                Cast(F('id'), output_field=CharField()),
+                5, Value('0')
+            )
+        ).values('code', 'id', 'name')
+
+
+def exercicio14():
+    return models.Employee.objects.annotate(
+        _upper=Upper('name'),
+        _lower=Lower('name')
+    ).values('_upper', '_lower')
+
+
+def exercicio15():
+    return models.Employee.objects.select_related('department').values('department__name', 'gender').annotate(
+        sum=Sum('salary')
+    ).values('department__name', 'sum', 'gender')
+
+
+# Fazer uma consulta para retornar o nome do produto, subtotal e quanto deve ser pago de comissão por cada item;
+# Fazer uma consulta para retornar o nome do produto, subtotal e quanto foi obtido de lucro por item;
+# Ranking dos 10 funcionários mais bem pagos;
+# Ranking dos 20 clientes que tem a menor renda mensal;
+# Trazer do décimo primeiro ao vigésimo funcionário mais bem pago;
+# Ranking dos produtos mais caros vendidos no ano de 2021;
+# Criar uma consulta para trazer o primeiro nome dos funcionários.
+#     Sr. Sra. Dr. Dra. remover se tiver.
+# Criar uma consulta para trazer o último nome dos clientes.
+# Criar uma consulta para rocar quem tenha silva no nome para Oliveira.
+# Criar uma consulta para trazer o total de funcionários por estado civil;
+# Criar uma consulta para trazer o total vendido em valor R$ por filial;
+# Criar uma consulta para trazer o total vendido em valor R$ por zona;
+# Criar uma consulta para trazer o total vendido em valor R$ por estado;
+# Criar uma consulta para trazer o total vendido em quantidade por cidade, trazer apenas as cidades que tiveram vendas acima de quantidade 100;
+# Trazer a media de salários por sexo, o sexo deve está de forma descritiva;
+# Fazer uma consulta que retorne os 5 grupos de produtos mais lucrativos em termos de valor, os grupos só entram na lista com lucros acima de R$ 200,00;
+# Uma consulta para trazer a média de salários por departamento;
+# Uma consulta para trazer o total vendido em valor por ano;
+# Uma consulta para trazer o total vendido em valor por idade de funcionário;
+
+
+def exercicio16():
+    # Fazer uma consulta para retornar o nome do funcionário e o bairro onde ele mora;
+    return models.Employee.objects.select_related('district').values('name', 'district__name')
+
+
+def exercicio17():
+    # Fazer uma consulta para retornar o nome do cliente, cidade e zona que o mesmo mora;
+    return models.Customer.objects.values(
+        'name', 'district__customer__name', 'district__zone__name'
+    )
+
+def exercicio18():
+    # Fazer uma consulta para retornar os dados da filial, estado e cidade onde a mesma está localizada;
+    return models.Branch.objects.values(
+        'name', 'district__name', 'district__city__state__name', 'district__city__name'
+    )
+
+
+def exercicio19():
+    # Fazer uma consulta par retornar os dados do funcionário, departamento onde ele trabalha e qual seu estado civil atual;
+    return models.Employee.objects.values(
+        'name', 'department__name', 'marital_status__name'
+    )
+
+
+def exercicio20():
+    # Fazer uma consulta para retornar o nome do produto vendido, o preço unitário e o subtotal;
+    return models.SaleItem.objects.annotate(
+        subtotal = ExpressionWrapper(F('quantity') * F('product__sale_price'), output_field=FloatField())
+    ).values(
+        'product__name', 'product__sale_price', 'subtotal'
+    )
+
+
+def exercicio21(commission=True):
+    field = 'product__product_group__commission_percentage'
+    if not commission:
+        field = 'product__product_group__gain_percentage'
+    return models.SaleItem.objects.select_related('product__product_group').annotate(
+        subtotal=ExpressionWrapper(F('quantity') * F('product__sale_price'), output_field=FloatField()),
+        commission_or_gain=ExpressionWrapper(
+            F('subtotal') * (F(field) / 100),
+            output_field=FloatField()
+        )
+    ).values(
+        'product__name', 'product__sale_price', 'subtotal',
+        'product__product_group__commission_percentage',
+        'commission_or_gain'
+    )
+
+
+def max():
+    # pega o salário de maior valor
+    return  models.Employee.objects.aggregate(max=Max('salary'))
+
+def min():
+    # pega o sário de minimo valor
+    return models.Employee.objects.aggregate(max=Min('salary'))
+
+def avg():
+    # pega a media
+    return models.Employee.objects.aggregate(max=Avg('salary'))
+
+def sum():
+    # soma todos os salarios
+    return models.Employee.objects.aggregate(max=Sum('salary'))
+
+def count():
+    # conta todos os itens
+    return models.Employee.objects.aggregate(max=Count('*'))
